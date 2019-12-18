c57bb1bf53c852ca39eaad323f667cfb3edac34d
asifaly <asifaly82@yahoo.com>
2019-12-18 22:29:33 +0530

    mockapi request added

--- app/controllers/documentary_credits_controller.rb
+++ app/controllers/documentary_credits_controller.rb
@@ -13,6 +13,32 @@ class DocumentaryCreditsController < ApplicationController
   # GET /documentary_credits/new
   def new
     @documentary_credit = DocumentaryCredit.new
+    @resp = Faraday.get "https://5df9eebb38678a00145f9aed.mockapi.io/api/v1/documentarycredit/1"
+    puts @resp.headers
+    if @resp.success?
+      body_hash = JSON.parse(@resp.body)
+      @documentary_credit.lc_number = body_hash["lc_number"]
+      @documentary_credit.issue_date = body_hash["issue_date"]
+      @documentary_credit.expiry_date = body_hash["expiry_date"]
+      @documentary_credit.currency = body_hash["currency"]
+      @documentary_credit.lc_amount = body_hash["amount"]
+      @documentary_credit.applicant_name = body_hash["applicant"]
+      @documentary_credit.beneficiary_name = body_hash["beneficiary"]
+      @documentary_credit.issuing_bank_name = body_hash["issuing_bank"]
+      @documentary_credit.tenor = body_hash["tenor"]
+      @documentary_credit.shipment_from = body_hash["shipment_from"]
+      @documentary_credit.port_of_loading = body_hash["port_of_loading"]
+      @documentary_credit.port_of_discharge = body_hash["port_of_discharge"]
+      @documentary_credit.final_destination = body_hash["final_destination"]
+      @documentary_credit.latest_shipment_date = body_hash["latest_shipment_date"]
+      @documentary_credit.goods_description = body_hash["goods_description"]
+      @documentary_credit.documents_required = body_hash["documents_required"]
+      @documentary_credit.additional_conditions = body_hash["additional_conditions"]
+      puts body_hash
+    else
+      @error = @resp.body
+      puts @error
+    end
   end
 
   # GET /documentary_credits/1/edit
